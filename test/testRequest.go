9f1dc4ccdd9e509d9fbcb877a2e23f1139b04e06
Alexander Sorokin <58079949+Soha-git@users.noreply.github.com>
2022-03-15 10:33:02 +0300

    fix: update .gitlab-ci.yaml

---
+++ test/testRequest.go
@@ -0,0 +1,26 @@
+package httptest_test
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+)
+
+func TestResponseRecorder() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	req := httptest.NewRequest("GET", "http://localhost", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+}
