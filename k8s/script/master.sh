c1fa6bba767159da17305976c7b7a88cfd938138
Alexander Sorokin <58079949+Soha-git@users.noreply.github.com>
2022-03-19 20:17:07 +0300

    fix: update .gitlab-ci.yaml

--- k8s/script/master.sh
+++ k8s/script/master.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 MASTER_IP="10.10.0.10"
+NODENAME=$(hostname -s)
+POD_CIDR="192.168.0.0/16"
 
-sudo kubeadm init --apiserver-advertise-address=$MASTER_IP
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get -y update
+sudo apt-get -y install helm
+helm version
+helm repo add gitlab https://charts.gitlab.io
+
+sudo kubeadm init --apiserver-advertise-address=$MASTER_IP  --apiserver-cert-extra-sans=$MASTER_IP --pod-network-cidr=$POD_CIDR --node-name $NODENAME
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
@@ -15,14 +25,91 @@ else
     mkdir -p $config_path
 fi
 
-cp -i /etc/kubernetes/admin.conf $config_path/config
+sudo cp -i /etc/kubernetes/admin.conf $config_path/config
 touch $config_path/join.sh
 chmod +x $config_path/join.sh       
 
 
 kubeadm token create --print-join-command > $config_path/join.sh
 
+cat << EOF > values.yaml
+## The GitLab Server URL (with protocol) that want to register the runner against
+## ref: https://docs.gitlab.com/runner/commands/README.html#gitlab-runner-register
+##
+gitlabURL: https://gitlab.com/
+
+## The Registration Token for adding new Runners to the GitLab Server. This must
+## be retreived from your GitLab Instance.
+## ref: https://docs.gitlab.com/ce/ci/runners/README.html#creating-and-registering-a-runner
+##
+runnerRegistrationToken: GR13489419beyAnswu4Z2ZWe6YyCB
+
+## Set the certsSecretName in order to pass custom certficates for GitLab Runner to use
+## Provide resource name for a Kubernetes Secret Object in the same namespace,
+## this is used to populate the /etc/gitlab-runner/certs directory
+## ref: https://docs.gitlab.com/runner/configuration/tls-self-signed.html#supported-options-for-self-signed-certificates
+##
+#certsSecretName:
+
+## Configure the maximum number of concurrent jobs
+## ref: https://docs.gitlab.com/runner/configuration/advanced-configuration.html#the-global-section
+##
+concurrent: 10
+
+## Defines in seconds how often to check GitLab for a new builds
+## ref: https://docs.gitlab.com/runner/configuration/advanced-configuration.html#the-global-section
+##
+checkInterval: 30
+
+## Configuration for the Pods that that the runner launches for each new job
+##
+runners:
+  ## Default container image to use for builds when none is specified
+  ##
+  image: ubuntu:16.04
+
+  ## Run all containers with the privileged flag enabled
+  ## This will allow the docker:dind image to run if you need to run Docker
+  ## commands. Please read the docs before turning this on:
+  ## ref: https://docs.gitlab.com/runner/executors/kubernetes.html#using-docker-dind
+  ##
+  privileged: false
+
+  ## Namespace to run Kubernetes jobs in (defaults to 'default')
+  ##
+  # namespace:
+
+  ## Build Container specific configuration
+  ##
+  builds:
+    # cpuLimit: 200m
+    # memoryLimit: 256Mi
+    cpuRequests: 100m
+    memoryRequests: 128Mi
+
+  ## Service Container specific configuration
+  ##
+  services:
+    # cpuLimit: 200m
+    # memoryLimit: 256Mi
+    cpuRequests: 100m
+    memoryRequests: 128Mi
+
+  ## Helper Container specific configuration
+  ##
+  helpers:
+    # cpuLimit: 200m
+    # memoryLimit: 256Mi
+    cpuRequests: 100m
+    memoryRequests: 128Mi
+
+EOF
+
+
 
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+kubectl create namespace gitlub-runner
+helm install --namespace gitlub-runner gitlab-runner -f values.yaml gitlab/gitlab-runner
--- k8s/script/node.sh
+++ k8s/script/node.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-rsync -av -e "ssh -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa" vagrant@10.10.0.10:/home/vagrant/join.sh /home/vagrant/
+rsync -av -e "ssh -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa" vagrant@10.10.0.10:/home/vagrant/config/* /home/vagrant/
 
 /bin/bash /home/vagrant/join.sh -v
+
+sudo -i -u vagrant bash << EOF
+mkdir -p /home/vagrant/.kube
+sudo cp -i /home/vagrant/config /home/vagrant/.kube/
+sudo chown 1000:1000 /home/vagrant/.kube/config
+NODENAME=$(hostname -s)
+kubectl label node $(hostname -s) node-role.kubernetes.io/worker=worker-new
+EOF
+
+sudo systemctl restart systemd-resolved
+sudo systemctl restart kubelet
 
 rm -R /home/vagrant/.ssh/id_rsa
