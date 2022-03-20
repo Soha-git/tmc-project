fdfceafc8c5176727303458bddf5c43df9faa4ce
Alexander Sorokin <58079949+Soha-git@users.noreply.github.com>
2022-03-20 12:24:22 +0300

    fix: update .gitlab-ci.yaml

--- k8s/script/master.sh
+++ k8s/script/master.sh
@@ -41,6 +41,84 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update 
 helm install ingress-nginx ingress-nginx/ingress-nginx
 
+cat <<EOF > values.yaml 
+## GitLab Runner Image
+##
+image: gitlab/gitlab-runner:alpine-v12.9.0
+
+## Specify a imagePullPolicy
+##
+imagePullPolicy: IfNotPresent
+
+## Default container image to use for initcontainer
+init:
+  image: busybox
+  tag: latest
+
+## The GitLab Server URL (with protocol) that want to register the runner against
+##
+gitlabUrl: https://gitlab.com/
+
+## The Registration Token for adding new Runners to the GitLab Server. This must
+## be retreived from your GitLab Instance.
+##
+runnerRegistrationToken: "GR13489419beyAnswu4Z2ZWe6YyCB"
+## Unregister all runners before termination
+##
+unregisterRunners: true
+
+## Configure the maximum number of concurrent jobs
+##
+concurrent: 10
+
+## Defines in seconds how often to check GitLab for a new builds
+##
+checkInterval: 30
+
+## For RBAC support:
+##
+rbac:
+  create: true
+  clusterWideAccess: false
+
+## Configure integrated Prometheus metrics exporter
+##
+metrics:
+  enabled: true
+
+## Configuration for the Pods that that the runner launches for each new job
+##
+runners:
+  ## Default container image to use for builds when none is specified
+  ##
+  image: ubuntu:16.04
+
+  ## Specify the tags associated with the runner. Comma-separated list of tags.
+  ##
+  tags: "k8s-runner"
+
+  ## Run all containers with the privileged flag enabled
+  ## This will allow the docker:dind image to run if you need to run Docker
+  ## commands. Please read the docs before turning this on:
+  ##
+  privileged: true
+
+  ## Namespace to run Kubernetes jobs in (defaults to the same namespace of this release)
+  ##
+  namespace: gitlab
+
+  cachePath: "/opt/cache"
+
+  cache: {}
+  builds: {}
+  services: {}
+  helpers: {}
+
+resources: {}
+EOF
+kubectl create namespace gitlab
+helm repo add gitlab https://charts.gitlab.io
+helm install --namespace gitlab  gitlab-runner -f values.yaml gitlab/gitlab-runner
 
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
