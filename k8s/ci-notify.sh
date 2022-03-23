cae1b886a6224e041fd908108c2047634450c7f7
Alexander Sorokin <58079949+Soha-git@users.noreply.github.com>
2022-03-23 15:12:25 +0300

    fix: update .gitlab-ci.yaml

--- k8s/ci-notify.sh
+++ k8s/ci-notify.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="$CI_JOB_STAGE status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="$1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
