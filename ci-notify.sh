8ef742eb546aa3c30c3aba54ded81a72cdfc664c
Alexander Sorokin <58079949+Soha-git@users.noreply.github.com>
2022-03-24 14:04:00 +0300

    fix: update .gitlab-ci.yaml

---
+++ ci-notify.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+token=$TELEGRAM_BOT_TOKEN 
+chat_id=$TELEGRAM_USER_ID    
+parse_mode=$PARSE_MODE
+message=$MESSAGE
+
+if [[ -z $token ]]
+then
+    echo 'Not passed required BOT_TOKEN environment variable' >&2
+    exit 1
+fi
+
+if [[ -z $chat_id ]]
+then
+    echo 'Not passed required CHAT_ID environment variable' >&2
+    exit 2
+fi
+
+if [[ -z $message ]]
+then
+    echo 'Not passed required MESSAGE environment variable' >&2
+    exit 3
+fi
+
+if [[ -z $parse_mode ]]
+then
+    parse_mode='Markdown'
+fi
+
+
+request="{\"text\":\"${message}\",\"parse_mode\":\"${parse_mode}\",\"chat_id\":${chat_id}}"
+
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -d "${request}" \
+     "https://api.telegram.org/bot${token}/sendMessage"
